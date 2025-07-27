dc5eadcd3d2842eb167a8b09bc8f9f0fa3bac555
Avinash <avin.roy001@gmail.com>
2025-07-27 22:38:14 +0530

    initiate

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="font-bold text-3xl bg-gray-500  
+   text-sky-300 max-w-screen min-h-screen overflow-hidden flex items-center justify-center"><span >Avinash roy</span></div>
+  )
+}
+
+export default App
